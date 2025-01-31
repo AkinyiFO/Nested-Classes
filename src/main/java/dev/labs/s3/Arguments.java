826d6a2120d1827f3152b4e8b92798fe14c8dce9
AkinyiFO <oafridah@gmail.com>
2025-01-31 13:13:42 +0300

    initial

---
+++ src/main/java/dev/labs/s3/Arguments.java
@@ -0,0 +1,74 @@
+package dev.labs.s3;
+
+public class Arguments {
+
+    public static void main(String[] args) {
+        Circle oval = new Circle(5,5);
+        System.out.println(Circle.numberOfCircles);
+
+        Circle round = new Circle(1,1);
+        System.out.println(Circle.numberOfCircles);
+    }
+}
+
+class OuterClass {
+ int z = 0;
+
+     class InnerClass {
+
+     static int y;
+     int x;
+
+     StaticNestedClass staticOb = new StaticNestedClass();
+
+    }
+    static class StaticNestedClass {
+        OuterClass ino = new OuterClass();
+        OuterClass.InnerClass inout = ino.new InnerClass();
+        int x = ino.z;
+        int z = inout.x;
+        StaticNestedClass staticOb = new StaticNestedClass();
+    }
+}
+
+// Ref: https://docs.oracle.com/javase/tutorial/java/javaOO/arguments.html
+class Circle {
+    private int originOne;
+    private int originTwo;
+
+    // Class Variable
+    public static int numberOfCircles;
+
+    static final double PI = 3.1416;
+
+
+    public Circle(int originOne, int originTwo) {
+        this.originOne = originOne;
+        this.originTwo = originTwo;
+        ++numberOfCircles;
+    }
+
+    public static int getNumberOfCircles(){
+//        return originOne; // Not possible
+//        int test = new Circle(2,2).originOne; // Work around
+        return numberOfCircles;
+    }
+
+    public int getOriginOne() {
+        return originOne;
+    }
+
+    public void setOriginOne(int originOne) {
+        this.originOne = originOne;
+    }
+
+    public int getOriginTwo() {
+        return originTwo;
+    }
+
+    public void setOriginTwo(int originTwo) {
+        this.originTwo = originTwo;
+    }
+
+
+}
---
+++ src/main/java/dev/labs/s3/Database.java
@@ -0,0 +1,64 @@
+package dev.labs.s3;
+
+/*
+Possible implementation of a Database
+ */
+
+class Database {
+
+    String connectionString = "";
+    String username = "";
+    private String password = "Changed!";
+
+    public Database() {
+    }
+
+    public Database(String startConnectionString, String startUsername, String startPassword) {
+        connectionString = startConnectionString;
+        username = startUsername;
+        password = startPassword;
+    }
+
+    // Reuse code
+    // Set default values
+    public Database(String username, String password){
+        this("url", username, password);
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    void connect(String newConnectionString, String newUsername, String newPassword) {
+        connectionString = newConnectionString;
+        username = newUsername;
+        password = newPassword;
+        System.out.println("Connected to the database with connection string: " + connectionString);
+    }
+
+    void disconnect() {
+        connectionString = "";
+        username = "";
+        password = "";
+        System.out.println("Disconnected from the database.");
+    }
+
+    void executeQuery(String query) {
+        if (!connectionString.isEmpty()) {
+            System.out.println("Executing query: " + query);
+            // Logic to execute query
+        } else {
+            System.out.println("No database connection. Please connect first.");
+        }
+    }
+
+    void printStatus() {
+        System.out.println("Connection String: " + connectionString +
+                " Username: " + username +
+                " Password: " + password);
+    }
+}
